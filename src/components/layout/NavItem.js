231451bf0c321e9edaf6e8a154b5bb4f0ad4900d
g-agrawal <er.gourav.agrawal@gmail.com>
2020-08-15 16:05:49 +0530

    toggle dropdown menu item

--- src/components/layout/NavItem.js
+++ src/components/layout/NavItem.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 
-function NavItem(props) {
-    const [open, setOpen] = useState(false);
-    return (
-        <li className="navbar-item">
-            {// eslint-disable-next-line
-            }<a href="#" className="navbar-item-btn" onClick={() => setOpen(!open)}>
-                { props.icon }
-            </a>
-            { open && props.children }
-        </li>        
-    );
+class NavItem extends Component {
+    state = {
+        open: false
+    }
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClick, false);
+    }
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClick, false);
+    }
+    handleClick = (e) => {
+        if(!this.node.contains(e.target)) {
+            this.setState({
+                open: false
+            });
+        }
+    }
+    render () {
+        return (
+            <li className="navbar-item" ref={node => this.node = node}>
+                {// eslint-disable-next-line
+                }<a href="#" className="navbar-item-btn" onClick={() => this.setState({open: !this.state.open})}>
+                    { this.props.icon }
+                </a>
+                { this.state.open && this.props.children }
+            </li>        
+        );
+    }
 }
 
 export default NavItem;
